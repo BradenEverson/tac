e95796816c68adb3e5e09ca852094fbe88408ad4
BradenEverson <bradeneverson@gmail.com>
2025-11-25 07:56:37 -0600

    Create generator

--- src/tac.zig
+++ src/tac.zig
@@ -1,11 +1,16 @@
 //! Three Address Codes for translating an AST into an IR
 
+const std = @import("std");
+
 const parser = @import("parser.zig");
 const Literal = parser.Literal;
 const BinaryOp = parser.BinaryOp;
+const Expr = parser.Expr;
 
 pub const ThreeAddressCode = union(enum) {
+    /// A raw value assignment to either a literal or a previous variable
     value: Literal,
+    /// A binary operation assignment
     binary: struct { left: Operand, op: BinaryOp, right: Operand },
 };
 
@@ -14,4 +19,15 @@ pub const Operand = union(enum) {
     literal: Literal,
     /// A reference to a previous TAC (i.e. t2 = t1 * 5)
     reference: usize,
+};
+
+pub const TacIrGenerator = struct {
+    ir_stream: std.ArrayList(ThreeAddressCode),
+    alloc: std.mem.Allocator,
+
+    ast: []const Expr,
+
+    pub fn generate(self: *TacIrGenerator) !void {
+        _ = self;
+    }
 };

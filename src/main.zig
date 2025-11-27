ea4fb4008aa7c7c0b9ab91600149746eb0cbb26c
BradenEverson <bradeneverson@gmail.com>
2025-11-26 20:53:32 -0600

    Tokenize, parse and generate TAC codes in main!

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,50 @@
 const std = @import("std");
 const tokenizer = @import("tokenizer.zig");
+const Token = tokenizer.Token;
+
+const parser = @import("parser.zig");
+const Expr = parser.Expr;
+const Parser = parser.Parser;
+
+const ir = @import("ir.zig");
+const TacIrGenerator = ir.TacIrGenerator;
+const ThreeAddressCode = ir.ThreeAddressCode;
 
 pub fn main() void {
-    std.debug.print("Let's make a Compiler\n", .{});
+    const alloc = std.heap.page_allocator;
+
+    var args = std.process.args();
+    _ = args.next(); // process name
+
+    if (args.next()) |path| {
+        const data = std.fs.cwd().readFileAlloc(alloc, path, std.math.maxInt(usize)) catch @panic("Failed to open file\n");
+        defer alloc.free(data);
+
+        var tokens = std.ArrayList(Token){};
+        defer tokens.deinit(alloc);
+
+        tokenizer.tokenize(data, &tokens, alloc) catch @panic("Failed to tokenize stream");
+
+        var parse = Parser.init(alloc, tokens.items);
+        defer parse.deinit();
+
+        var ast = std.ArrayList(*const Expr){};
+
+        parse.parse(&ast) catch @panic("Failed to parse tokens :(");
+
+        var ir_generator = TacIrGenerator.init(alloc, ast.items);
+        defer ir_generator.deinit();
+
+        ir_generator.generate() catch @panic("Failed to generate IR");
+
+        const tac = ir_generator.ir_stream.items;
+
+        for (tac) |instr| {
+            std.debug.print("{any}\n", .{instr});
+        }
+    } else {
+        std.debug.print("Usage: ./tac {{file}}\n", .{});
+    }
 }
 
 test {

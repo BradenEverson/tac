d898dc19a55586a24c2135d461cca5528ba55e01
BradenEverson <bradeneverson@gmail.com>
2025-11-26 21:04:14 -0600

    Stub out assembler

--- src/assembler.zig
+++ src/assembler.zig
@@ -1,1 +1,24 @@
 //! Translation between TAC IR and x86-64 Assembly
+
+const std = @import("std");
+
+const ir = @import("ir.zig");
+const ThreeAddressCode = ir.ThreeAddressCode;
+
+pub const Assembler = struct {
+    ir: []ThreeAddressCode,
+    fd: std.fs.File,
+
+    pub fn init(name: []const u8, codes: []ThreeAddressCode) !Assembler {
+        const fd = try std.fs.cwd().createFile(name, .{});
+        return Assembler{ .ir = codes, .fd = fd };
+    }
+
+    pub fn deinit(self: *Assembler) void {
+        self.fd.close();
+    }
+
+    pub fn translate(self: *Assembler) void {
+        _ = self;
+    }
+};
